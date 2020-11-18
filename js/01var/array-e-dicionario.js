b2de5d7bac2536272f10095f5ba0fbfd867f18ae
Leonardo Alves <leonardoalves1992@outlook.com>
2020-11-18 20:51:09 -0300

    02aula-array e Dicionarios

---
+++ js/01var/array-e-dicionario.js
@@ -0,0 +1,21 @@
+/*
+var lista = ["maça", "pera", "laranja"]
+//lista.push("limão");//add item novo a meu array
+//lista.pop(); //se quiser tirar o ultimo elemento
+console.log(lista);//lista todos os intens do array
+//console.log(3);//listar item especifico do array
+console.log(lista.length);//consigo ver o tamanho da lista
+console.log(lista.reverse());//listar itens ao contrario
+console.log(lista.toString());//perde a referencia de array e tras para string
+console.log(lista.join(" - "));//transforma em striga mais acrecenta o traço*/
+//-----------------------------------------------------------
+
+/*ADICIONANDO O DICIONARIO */
+/*
+var fruta = {nome:"maça", cor:"vermelho"} //dicionario
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+var frutas = [{nome:"maça", cor:"vermelho"}, {nome:"uva", cor:"roxa"}] //lista de dicionarios
+console.log(frutas);
+alert(frutas[1].nome);
